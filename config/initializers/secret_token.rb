6c0e164cc730fe4acbb5a4bf68f98e61c94786a4
Isaac Elias <nicecurelom@gmail.com>
2013-07-04 18:22:03 -0700

    I've installed all of the testing gems required by Hartl's Rails Tut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangle::Application.config.secret_token = '226f5312e514f4eddf61020443ab24c1c3b348a02dc213bad5f981f31021e8dc07dcaf86b7e9d81061f5fbcf9c8dcccac4d40c6136d86234e79ae3735d3d703a'
